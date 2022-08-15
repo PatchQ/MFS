7c8b7cbaac1994f44d1af521451e8a352ec91d3b
patchq <patchq@gmail.com>
2022-08-16 00:01:43 +0800

    update

---
+++ getStockData.py
@@ -0,0 +1,31 @@
+
+import pandas as pd
+import numpy as np
+import openpyxl
+from datetime import datetime, timedelta
+import yfinance as yf
+
+stocklist = pd.read_excel("outputlist.xlsx",dtype=str)
+outputlist = pd.DataFrame()
+
+for sno in stocklist["股票編號"][:]:
+    tempsno = str(sno).lstrip("0")
+    tempsno = tempsno.zfill(7)
+
+    print(sno)
+
+    msft = yf.Ticker(tempsno)
+    outputlist = msft.history(period="max")
+    outputlist = outputlist.reset_index()
+    outputlist["Date"] = outputlist["Date"].dt.strftime("%Y-%m-%d")
+
+    outputlist.to_excel("../StockData/"+sno+".xlsx",index=False)
+
+
+
+
+
+
+
+    
+   
