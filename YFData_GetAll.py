4bcd8a4d23e73d441f56f1630c784a963d6b90c9
Patch <patchq@gmail.com>
2025-08-14 20:58:54 +0800

    20250814

--- YFData_GetAll.py
+++ YFData_GetAll.py
@@ -6,10 +6,11 @@ import concurrent.futures as cf
 from tqdm import tqdm
 
 
-PATH = "../SData/YFData/"
-#SDATE = "2021-01-01"
-SDATE = "1980-01-01"
+PATH = "../../SData/YFData/"
+SDATE = "2024-01-01"
+#SDATE = "1980-01-01"
 EDATE = (datetime.today() + timedelta(days=1)).strftime("%Y-%m-%d")
+
 
 STOCKLIST_L = pd.read_excel("Data/stocklist_L.xlsx",dtype=str)
 STOCKLIST_M = pd.read_excel("Data/stocklist_M.xlsx",dtype=str)
@@ -30,14 +31,14 @@ SLIST_S = SLIST_S.assign(type="S")
 SLIST = pd.concat([SLIST_L, SLIST_M], ignore_index=True)
 SLIST = pd.concat([SLIST, SLIST_S], ignore_index=True)
 
-SLIST = SLIST[:]
+SLIST = SLIST[:1]
 
 
 def getData(sno,type):    
     outputlist = yf.download(sno, threads=False, interval='1d', auto_adjust=True, start=SDATE, end=EDATE, progress=False, show_errors=False)
     outputlist.insert(0,"sno", sno)
     outputlist = outputlist.loc[outputlist["Volume"]>0]
-    outputlist.to_excel(PATH+type+"/"+sno+".xlsx")
+    outputlist.to_excel(PATH+"/"+sno+".xlsx")
 
 def main():
     with cf.ProcessPoolExecutor(max_workers=17) as executor:
@@ -51,7 +52,10 @@ def main_ipad():
 if __name__ == '__main__':
     start = t.perf_counter()
 
-    main()
+    #main_ipad()
+    hk005 = yf.Ticker("0005.HK")
+    outputlist = hk005.history(period="1mo")
+    print(outputlist)
 
     finish = t.perf_counter()
     print(f'It took {round(finish-start,2)} second(s) to finish.')
