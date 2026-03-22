0897de84e62c5e3b5451fa0239758f90421d08e7
patchq <patchq@gmail.com>
2026-03-23 01:33:46 +0800

    20260322

--- HKEX/StockOption.py
+++ HKEX/StockOption.py
@@ -144,8 +144,8 @@ if __name__ == "__main__":
     sdate = cc.getLastWorkday(cc.date.today()).strftime("%Y%m%d")
     edate = cc.getLastWorkday(cc.date.today()).strftime("%Y%m%d")
 
-    sdate = "20260202"
-    edate = "20260202"
+    sdate = "20260316"
+    edate = "20260320"
 
     ProcessDownlaod(sdate, edate)
     ProcessExtract(sdate, edate)
